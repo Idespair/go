ec0efa8d6a1bd358760be666ca3f577056884edd
Idespair <kawanptbr@gmail.com>
2025-06-02 01:22:47 -0300

    French testing added

--- Hello.go
+++ Hello.go
@@ -3,6 +3,8 @@ package main
 // import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
@@ -11,6 +13,10 @@ func Hello(name string, language string) string {
 
 	if language == "Spanish" {
 		return "Hola, " + name
+	}
+
+	if language == "French" {
+		return "Bonjour, " + name
 	}
 
 	return englishHelloPrefix + name
--- hello_test.go
+++ hello_test.go
@@ -23,6 +23,12 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Salma", "French")
+		want := "Bonjour, Salma"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {

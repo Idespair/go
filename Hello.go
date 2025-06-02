bbbd4323b387e09e7879bc758a661e9b3b87db5a
Idespair <kawanptbr@gmail.com>
2025-06-02 01:12:51 -0300

    Spanish test added

--- Hello.go
+++ Hello.go
@@ -1,17 +1,21 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 
 	return englishHelloPrefix + name
 }
 
-func main() {
+/* func main() {
 	fmt.Println("Hello, World!")
-}
+} */
